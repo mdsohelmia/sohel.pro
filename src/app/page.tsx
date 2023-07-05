4713f87a606e0289ea93a567081157d78d10c01d
Sohel Mia <sohelcse1999@gmail.com>
2023-07-06 03:30:37 +0600

    add player ifrem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Player</h1>
+    <main className="w-[300px] h-screen mx-auto">
       <iframe
+        className="w-full h-full"
         src="https://cdn.dhakalive.tv/jsplayer.html"
         title="Gotipath Live"
       ></iframe>
