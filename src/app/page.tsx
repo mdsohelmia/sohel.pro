f62913bc18f87dcad1ec78804796393957f18167
Sohel Mia <sohelcse1999@gmail.com>
2023-07-06 03:17:53 +0600

    add player ifrem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <iframe
+      <iframe
         src="https://cdn.dhakalive.tv/jsplayer.html"
         title="Gotipath Live"
       ></iframe>
     </main>
-  )
+  );
 }
